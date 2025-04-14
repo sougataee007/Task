b1126343421e98bb95f32886c209a63768c740eb
sougataee007 <sougataee007@gmail.com>
2025-04-14 13:53:08 +0530

    Add files via upload

---
+++ Basic_Arithmatic.py
@@ -0,0 +1,22 @@
+#Task 1: Perform Basic Mathematical Operations
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+#o	Subtraction
+#o	Multiplication
+#o	Division
+#3.  Displays the results of each operation on the screen.
+
+x = input("Enter the First number : ")
+y = input("Enter the Second number : ")
+
+#Addition
+print("Addition : ", int(x) + int(y))
+#Subtraction
+print("Subtraction : ", int(x) - int(y))
+#Multiplication
+print("Multiplication : ", int(x) * int(y))
+#Division
+print("Division : ", float(x) / float(y))
+
