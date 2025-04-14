060054fee36495f812d13fcada49728e957f0fbf
sougataee007 <sougataee007@gmail.com>
2025-04-14 14:01:53 +0530

    Add files via upload

---
+++ Personalized_Greeting.py
@@ -0,0 +1,10 @@
+#Task 2: Create a Personalized Greeting
+#Problem Statement: Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+#2.  Concatenates the first name and last name into a full name.
+#3.  Prints a personalized greeting message using the full name.
+
+First_name = input("Enter your first name : ")
+Last_name = input("Enter your second name : ")
+
+print(f"Hello {First_name} {Last_name}! Welcome to the python program.")
